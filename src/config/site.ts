85052fd555e576e1a6045ce204f2f186c7eaa46d
ChoeYooJeong <cheez1248@gmail.com>
2026-07-18 15:54:43 +0900

    수정: 직무와 메인 소개 문구 변경

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,8 +5,8 @@ export const siteConfig = {
   email: 'cheezmicro@gmail.com',
   github: 'https://github.com/cheezcyj',
   description:
-    '웹디자인, 웹퍼블리싱, 프론트엔드 프로젝트와 배운 내용을 기록하는 개인 포트폴리오 아카이브.',
-  roles: ['Web Designer', 'Web Publisher', 'Frontend Engineer'],
+    '웹디자인, 웹퍼블리싱, 개발 프로젝트와 배운 내용을 기록하는 개인 포트폴리오 아카이브.',
+  roles: ['Web Designer', 'Web Publisher', 'Fullstack Engineer'],
 } as const
 
 export const navigation = [
